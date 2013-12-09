46d5a90a19f46a87663d549202a254e22ed2986a
Engineer <engineer@engineer-ThinkCentre-Edge-71z.(none)>
2013-12-09 17:00:56 +0530

    devise work for login and signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-	has_secure_password
-  attr_accessible :email, :password, :password_confirmation
+ # Setup accessible (or protected) attributes for your model
+ # has_secure_password
+ attr_accessible :email, :password, :password_confirmation, :remember_me
+	
+ # attr_accessible :email, :password, :password_confirmation
   validates_uniqueness_of :email
 end
