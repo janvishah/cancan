07c457f3cfe05c633dd9886a1e55f6f0dc696e62
Engineer <engineer@engineer-ThinkCentre-Edge-71z.(none)>
2013-12-10 15:37:43 +0530

    redirect a specific page after signin or signout using devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  private
- 
+  
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || homes_path
+  end  
+
+  def after_sign_out_path_for(resource_name)
+	  # logic here
+	  root_path
+	end
 end
