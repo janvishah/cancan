a37e37216ff5ffd8a3864b9df5304f98f3757941
Engineer <engineer@engineer-ThinkCentre-Edge-71z.(none)>
2013-12-10 10:43:21 +0530

    devise work for login and signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable, :timeoutable and :om	niauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
  # Setup accessible (or protected) attributes for your model
- # has_secure_password
+
  attr_accessible :email, :password, :password_confirmation, :remember_me
 	
- # attr_accessible :email, :password, :password_confirmation
+ 
   validates_uniqueness_of :email
 end
